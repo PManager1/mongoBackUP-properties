eb25092c5deaee01f547e47933e85c1f2e0d84a9
jpca999 <jpca999@gmail.com>
2016-11-29 11:57:18 -0500

    -

--- commands.js
+++ commands.js
@@ -1,12 +1,24 @@
 
-# ADD 
+# ADD   | DEV
 mongoimport --db mean-dev --collection properties --jsonArray --file /home/ubuntu/MongoBackUP-properties/new_properties.json
 mongoimport --db mean-dev --collection users --jsonArray --file new_users.json
 
+# ADD   | PROD
+mongoimport --db mean --collection properties --jsonArray --file /home/ubuntu/MongoBackUP-properties/new_properties.json
+mongoimport --db mean --collection users --jsonArray --file new_users.json
 
-# ADD and DROP
+
+
+
+# ADD and DROP  | DEV 
 mongoimport --db mean-dev --collection properties --drop --jsonArray --file /home/ubuntu/MongoBackUP-properties/new_properties.json
 mongoimport --db mean-dev --collection users --drop --jsonArray --file new_users.json
+
+
+# ADD and DROP  | PROD
+mongoimport --db mean --collection properties --drop --jsonArray --file /home/ubuntu/MongoBackUP-properties/new_properties.json
+mongoimport --db mean --collection users --drop --jsonArray --file new_users.json
+
 
 
 # LOCAL - MAC
