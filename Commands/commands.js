2ae6bb7e18388ae6d8454acb6cf34b57149ff09d
jpca999 <jpca999@gmail.com>
2016-12-12 18:02:42 -0500

    -

---
+++ Commands/commands.js
@@ -0,0 +1,74 @@
+
+# ADD and DROP  | PROD
+mongoimport --db mean --collection properties --drop --jsonArray --file /home/ubuntu/MongoBackUP-properties/new_properties.json
+mongoimport --db mean --collection users --drop --jsonArray --file new_users.json
+
+
+# ADD  ONLY   | PROD
+mongoimport --db mean --collection properties --jsonArray --file /home/ubuntu/MongoBackUP-properties/new_properties.json
+mongoimport --db mean --collection users --jsonArray --file new_users.json
+
+
+
+
+
+# ADD  ONLY   | DEV
+mongoimport --db mean-dev --collection properties --jsonArray --file /home/ubuntu/MongoBackUP-properties/new_properties.json
+mongoimport --db mean-dev --collection users --jsonArray --file new_users.json
+
+
+
+
+# ADD and DROP  | DEV 
+mongoimport --db mean-dev --collection properties --drop --jsonArray --file /home/ubuntu/MongoBackUP-properties/new_properties.json
+mongoimport --db mean-dev --collection users --drop --jsonArray --file new_users.json
+
+
+
+
+#  EXPORT #  EXPORT #  EXPORT #  EXPORT #  EXPORT #  EXPORT #  EXPORT 
+
+
+# PROD 
+
+mongoexport --jsonArray --db mean --collection properties  --out export/export-12-12.json
+
+
+
+# LOCAL - MAC
+
+mongoexport --jsonArray --db mean-dev --collection properties  --out new_properties.json
+mongoexport --jsonArray --db mean-dev --collection users  --out new_users.json
+
+mongoexport --jsonArray --db mean-dev --collection articles  --out export-articles.json
+
+# LOCAL IMPORT 
+
+mongoimport --db mean-dev --collection properties --drop --jsonArray --file export-properties.json
+
+mongoexport --jsonArray --db mean-dev --collection properties  --out export-properties.json
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
