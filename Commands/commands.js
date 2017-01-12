12aae5722134fc197fe00ada7880d1107d33f15b
jpca999 <jpca999@gmail.com>
2017-01-12 09:44:54 -0500

    -

--- Commands/commands.js
+++ Commands/commands.js
@@ -1,5 +1,3 @@
-
-
 # ADD  ONLY   | PROD
 
 mongoimport --db mean --collection properties --jsonArray --file 
@@ -43,7 +41,7 @@ mongoimport --db mean-dev --collection users --drop --jsonArray --file new_users
 #  EXPORT #  EXPORT #  EXPORT #  EXPORT #  EXPORT #  EXPORT #  EXPORT 
 # PROD 
 
-mongoexport --jsonArray --db mean --collection properties  --out export/export-1-12.json
+mongoexport --jsonArray --db mean --collection properties  --out export/export-1-12-3.json
 
 
 
